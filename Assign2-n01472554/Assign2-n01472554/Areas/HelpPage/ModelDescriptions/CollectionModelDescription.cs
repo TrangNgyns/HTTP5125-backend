1c8ef313a2bb8aa4b3ede5d088512dd8e28c37f2
Kevin <127543991+TrangNgyns@users.noreply.github.com>
2024-06-25 04:50:45 -0400

    Delete Assign2 dir from branch
    assign2 files are already in assign2 branch, so out they go from assign1 branch

--- Assign2-n01472554/Assign2-n01472554/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Assign2_n01472554.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
